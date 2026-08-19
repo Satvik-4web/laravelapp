7152b08a8950b9df67b2b070b62a5ba1136caf12
Satvik <sganda_be24@thapar.edu>
2026-08-20 00:54:15 +0530

    Implement Sanctum authentication API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('students', StudentController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('students', StudentController::class);
+});
